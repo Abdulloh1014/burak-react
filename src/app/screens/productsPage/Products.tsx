a5b3e94923abb7e30a25540a8287ad31e4d14565
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-12-09 19:43:54 +0900

    fix: retreive data and modify products component

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Button, Container, Stack } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -15,6 +15,9 @@ import { setProducts } from "./slice";
 import { createSelector } from "reselect";
 import { retrieveProducts } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+import { serverApi } from "../../../lib/config";
 
 
 /** REDUX SLICE & SELECTOR   */
@@ -27,20 +30,24 @@ const productsRetriever = createSelector(
   (products) => ({products})
 );
 
-
-const products = [
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    
-];
-
 export default function Products() {
+
+    const { setProducts } = actionDispatch(useDispatch());
+     const {products} = useSelector(productsRetriever);
+
+
+    useEffect(() => {
+      const product = new ProductService();
+      product.getProducts({
+        page: 1,
+        limit: 8,
+        order: "createdAt",
+        productCollection: ProductCollection.DISH,
+        search: "",
+      }).then((data) => setProducts(data))
+        .catch((err) => console.log(err));
+    }, [])
+
     return (
         <div className={"products"}>
             <Container>
@@ -145,15 +152,20 @@ export default function Products() {
 
                         <Stack className={"product-wrapper"}>
                             {products.length !== 0 ? (
-                                products.map((product, index) => {
+                                products.map((product: Product) => {
+                                    const imagePath = `${serverApi}/${product.productImages[0]}`;
+                                    const sizeVolume = 
+                                    product.productCollection === ProductCollection.DRINK
+                                    ? product.productCollection + " litre"
+                                    : product.productSize + " size";
                                     return (
-                                        <Stack key={index} className={"product-card"}>
+                                        <Stack key={product._id} className={"product-card"}>
                                             <Stack 
                                             className={"product-img"}
-                                            sx={{ backgroundImage: `url(${product.imagePath})`,  }}
+                                            sx={{ backgroundImage: `url(${imagePath})`,  }}
                                             >
                                             
-                                            <div className={"product-sale"}>Normal size</div>
+                                            <div className={"product-sale"}>{sizeVolume}</div>
                                             <Button>
                                                 <img 
                                                 className="shopping-icon"
@@ -162,28 +174,27 @@ export default function Products() {
                                                 />
                                             </Button>
                                             <Button className={"view-btn"} sx={{right: "36px"}}>
-                                                <Badge badgeContent={27} color="secondary">
+                                                <Badge badgeContent={product.productViews} color="secondary">
                                                     <RemoveRedEyeIcon
-                                                    sx={{color: 25 ? "gray" : "red", }}
+                                                    sx={{color: 
+                                                        product.productViews === 0 ? "gray" : "white", }}
                                                       />
                                                 </Badge>
                                                 
                                             </Button>
 
                                             </Stack> 
-                                            <Box>
+                                            <Box className={"product-narx"}>
                                                 <span className={"product-title"}>
                                                     {product.productName}
                                                 </span>
 
-                                                <Box className={"narx"}>
-                                                     <div>
-                                                    <MonetizationOnIcon sx={{ color: "#d7b686" }} />
-                                                        
+                                               <div className={"product-www"}>
+                                                    <MonetizationOnIcon />
+                                                        {product.productPrice}
                                                 </div>
-                                                <div className={"sena"}>  {12}</div>
 
-                                                </Box>
+                                                
 
                                                
                                             </Box>
