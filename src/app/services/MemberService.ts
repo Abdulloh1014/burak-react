a477a3c7522aa1e3b89b45ab74f5a9b6b14f1bc0
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-12-12 23:34:55 +0900

    fix: create login process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,8 +1,6 @@
-
 import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member } from "../../lib/types/member";
-
+import { LoginInput, Member, MemberInput } from "../../lib/types/member";
 
 class MemberService {
     private readonly path: string;
@@ -12,32 +10,67 @@ class MemberService {
     }
 
     public async getTopUsers(): Promise<Member[]> {
-        try{
+        try {
             const url = this.path + "/member/top-users";
             const result = await axios.get(url);
 
             console.log("getTopUsers:", result)
             return result.data;
-        } catch(err){
-           console.log("Error, getTopUsers:", err);
-           throw err;
+        } catch (err) {
+            console.log("Error, getTopUsers:", err);
+            throw err;
         }
     }
 
     public async getRestaurant(): Promise<Member> {
-        try{
+        try {
             const url = this.path + "/member/restaurant";
             const result = await axios.get(url);
-            console.log("getRestaurant:", result)
+            console.log("getRestaurant:", result);
 
             const restaurant: Member = result.data;
             return restaurant;
-        } catch(err){
-           console.log("Error, getRestaurant:", err);
-           throw err;
+        } catch (err) {
+            console.log("Error, getRestaurant:", err);
+            throw err;
         }
     }
 
+    public async signup(input: MemberInput): Promise<Member> {
+        try {
+            const url = this.path + "/member/signup";
+            const result = await axios.post(url, input, { withCredentials: true });
+            console.log("signup", result);
+
+            const member: Member = result.data.member;
+            console.log("member:", member);
+            localStorage.setItem("memberData", JSON.stringify(member));
+
+            return member;
+
+        } catch (err) {
+            console.log("Error, signup:", err);
+            throw err;
+        }
+    }
+
+    public async login(input: LoginInput): Promise<Member> {
+        try {
+            const url = this.path + "/member/login";
+            const result = await axios.post(url, input, { withCredentials: true });
+            console.log("login", result);
+
+            const member: Member = result.data.member;
+            console.log("member:", member);
+            localStorage.setItem("memberData", JSON.stringify(member));
+
+            return member;
+
+        } catch (err) {
+            console.log("Error, login:", err);
+            throw err;
+        }
+    }
 }
 
 export default MemberService;
