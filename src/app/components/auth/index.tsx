5a0a23c9b894d68b30a35158e4b59b5b2f987685
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-12-13 19:06:39 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -11,6 +11,7 @@ import { LoginInput, MemberInput } from "../../../lib/types/member";
 import { Messages } from "../../../lib/config";
 import MemberService from "../../services/MemberService";
 import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import { useGlobals } from "../../hooks/useGlobals";
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: "flex",
@@ -47,6 +48,7 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
   const [memberNick, setMemberNick] = useState<string>("");
   const [memberPhone, setMemberPhone] = useState<string>("");
   const [memberPassword, setMemberPassword] = useState<string>("");
+  const { setAuthMember } = useGlobals();
 
   /** HANDLERS **/
 
@@ -85,7 +87,8 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
       const result = await member.signup(signupInput);
 
      // Saving Authenticated user
-
+      
+     setAuthMember(result);
       handleSignupClose();
     } catch(err) {
       console.log(err);
@@ -110,7 +113,7 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
 
        // Saving Authenticated user
 
-
+      setAuthMember(result);
       handleLoginClose();
     } catch(err) {
       console.log(err);
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,8 +1,11 @@
-import { Box, Container, Stack, Button } from "@mui/material";
+import { Box, Container, Stack, Button, Menu, MenuItem, ListItemIcon } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
 import React, {useState, useEffect} from "react";
 import { CartItem } from "../../../lib/types/search";
+import { useGlobals } from "../../hooks/useGlobals";
+import { serverApi } from "../../../lib/config";
+import { Logout } from "@mui/icons-material";
 
 
 interface HomeNavbarProps {
@@ -13,15 +16,27 @@ interface HomeNavbarProps {
     onDeleteAll: () => void;
     setSignupOpen: (isOpen: boolean) => void;
     setLoginOpen: (isOpen: boolean) => void;
+    handleLogoutClick: (e: React.MouseEvent<HTMLElement>) => void;
+    anchorEl: HTMLElement | null;
+    handleCloseLogout: () => void;
+    handleLogoutRequest: () => void;
 }
 
 export default function HomeNavbar (props: HomeNavbarProps) {
       const { 
         cartItems, 
-        onAdd, onRemove, onDelete, onDeleteAll, 
-        setSignupOpen, setLoginOpen 
+        onAdd, 
+        onRemove, 
+        onDelete, 
+        onDeleteAll, 
+        setSignupOpen, 
+        setLoginOpen,
+        handleLogoutClick,
+        anchorEl,
+        handleCloseLogout,
+        handleLogoutRequest
     } = props;
-    const  authMember  = null;
+  const { authMember } = useGlobals();
     
 
     /** Handlers */
@@ -80,14 +95,62 @@ export default function HomeNavbar (props: HomeNavbarProps) {
            ) : (
             <img 
             className="user-avatar"
-            src={"/icons/default-user.svg"}
+            src={
+                authMember?.memberImage
+                ? `${serverApi}/${authMember?.memberImage}`
+                : "/icons/default-user.svg"}
             aria-haspopup={"true"}
+            onClick={handleLogoutClick}
             />
            )}
 
+<Menu
+   anchorEl={anchorEl}
+	id="account-menu"
+    open={Boolean(anchorEl)}
+    onClose={handleCloseLogout}
+    onClick={handleCloseLogout}
+	PaperProps={{
+		elevation: 0,
+		sx: {
+			overflow: 'visible',
+			filter: 'drop-shadow(0px 2px 8px rgba(109, 81, 81, 0.32))',
+			mt: 1.5,
+			'& .MuiAvatar-root': {
+				width: 32,
+				height: 32,
+				ml: -0.5,
+				mr: 1,
+			},
+			'&:before': {
+				content: '""',
+				display: 'block',
+				position: 'absolute',
+				top: 0,
+				right: 14,
+				width: 10,
+				height: 10,
+				bgcolor: 'background.paper',
+				transform: 'translateY(-50%) rotate(45deg)',
+				zIndex: 0,
+			},
+		},
+	}}
+	transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+	anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+>
+	<MenuItem onClick={handleLogoutRequest}>
+		<ListItemIcon>
+			<Logout fontSize="small" style={{ color: 'blue' }} />
+		</ListItemIcon>
+		Logout
+	 </MenuItem>
+   </Menu>
+ </Stack>
 
-           </Stack>
-        </Stack>
+
+
+     </Stack>
         <Stack className={"header-frame"}>
             <Stack className={"detail"}>
 
